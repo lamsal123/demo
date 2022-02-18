7360d137ea3808dd5174d2756e8ae38fbefdcb4d
lamsal123 <99975435+lamsal123@users.noreply.github.com>
2022-02-18 09:40:32 -0500

    Add files via upload

---
+++ ArrayClass.java
@@ -0,0 +1,25 @@
+package javaPackage;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] a= {56 , 75, 87, 78, 98, 99};
+		
+		String [] b= {"Puja","Ravi"};
+		
+		System.out.println(a[5]);
+		System.out.println(a[0]);
+		
+		System.out.println(a.length);
+		
+		System.out.println(b[0]);
+		
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+	
+}
